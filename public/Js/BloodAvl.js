e314f06aff000e6288a942e25bd73a44add1c64e
sambit kumar pradhan <s4sambit632@gmail.com>
2023-12-01 11:21:02 +0530

    last commit

---
+++ public/Js/BloodAvl.js
@@ -0,0 +1,6 @@
+const Anguldist = document.querySelector("#Angul"),
+  contaner = document.querySelector(".contaner");
+
+Anguldist.addEventListener("click", () => contaner.classList.add("cont"));
+
+alert("Hello! I am an alert box!!");
